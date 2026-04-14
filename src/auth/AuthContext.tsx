a1b75573595021f26f723187dd52607fe1397dca
ignaciodipaolo <nachobeatbox@gmail.com>
2026-04-14 00:50:11 -0300

    fix: update api url to production

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -39,7 +39,7 @@ export const AuthProvider = ({ children }: Props) => {
 
   const login = async (email: string, password: string): Promise<boolean> => {
     try {
-      const res = await fetch("http://localhost:3201/api/login", {
+      const res = await fetch("https://chatup-api.dipaoloproyects.space/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
